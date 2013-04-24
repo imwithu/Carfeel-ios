e4313f5bff7661fe78b5901ceadf5618ce3f6c36
James Yu <james@withu.im>
2013-04-23 23:26:54 -0700

    Initial Commit

---
+++ Carfeel/CFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CFAppDelegate.h
+//  Carfeel
+//
+//  Created by James Yu on 4/23/13.
+//  Copyright (c) 2013 Jianqiang Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
